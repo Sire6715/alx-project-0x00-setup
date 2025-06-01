105d61b54f31b20b806ff5b26faa1236069fa125
Prince <uchepibe@gmail.com>
2025-06-01 14:43:55 +0100

    Setting up project directories

---
+++ alx-project-0x00/pages/index.tsx
@@ -0,0 +1,10 @@
+const Home: React.FC = () => {
+  return(
+    <main className="flex min-h-screen flex-col items-center justify-between text-4xl font-semibold">
+      <h1>Airbnb Application clone system</h1>
+      <button className="bg-blue-400 text-white rounded-full py-1 px-3 text-lg mt-3">Get started</button>
+    </main>
+  )
+}
+
+export default Home;
